48fac15d9117aa4cd6102b1862b4a6dc3561b3df
ajg7 <ajgebara@gmail.com>
2020-09-03 23:05:25 -0500

    second test passed

--- src/components/ContactForm.test.js
+++ src/components/ContactForm.test.js
@@ -1,8 +1,15 @@
 import React from "react";
 import {render, screen, fireEvent} from "@testing-library/react";
 import ContactForm from "./ContactForm";
+import { act } from "react-dom/test-utils";
 
 
-test("ContactForm is renders a contact", () => {
+test("ContactForm renders", () => {
     render(<ContactForm />)
+})
+
+test("email renders", () => {
+    const { getByTestId } = render(<ContactForm />);
+    const emailInput = getByTestId ("email");
+    expect(emailInput.textContent).toBe("");
 })
